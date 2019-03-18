92c00e949eafa9fd5e39294d7e6357dd0f6510e2
jeffau13 <jeffau13@gmail.com>
2019-03-18 18:16:06 -0400

    added routes for answers

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require('express');
 const app = express();
 const routes = require('./routes.js');
 const jsonParser = require('body-parser').json;
+const logger = require('morgan');
 
 const port = process.env.PORT || 3000;
 
+app.use(logger('dev'));
 app.use(jsonParser());
 
 app.use("/questions", routes);
--- routes.js
+++ routes.js
@@ -20,13 +20,55 @@ router.post('/', (req, res, next) => {
     res.json(testobj);
 });
 
-//GET /question/:id:
-router.get('/:id', (req,res,next)=>{
+//GET /question/:qID:
+router.get('/:qID', (req,res,next)=>{
     let testobj = { response: 'You sent a GET request!',
-    id: req.params.id
+    id: req.params.qID
 
     };
     res.json(testobj);
 });
 
+// POST /questions/:id/answers
+// Route for creating an answer
+router.post("/:qID/answers", (req, res,next)=>{
+	res.json({
+		response: "You sent a POST request to /answers",
+		questionId: req.params.qID,
+		body: req.body
+	});
+});
+
+//PUT /questions/:id/answers/:id
+// Edit a specific answer
+router.put("/:qID/answers/:aID",(req,res,next)=>{
+    res.json({
+        response: "You sent a PUT request to /answers",
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        body: req.body
+    });
+});
+
+//DELeTE /questions/:id/answers/:id
+// DELETE a specific answer
+router.delete("/:qID/answers/:aID",(req,res,next)=>{
+    res.json({
+        response: "You sent a DELETE request to /answers",
+        questionId: req.params.qID,
+        answerId: req.params.aID
+    });
+});
+
+//Post /questions/:id/answers/:aID/vote-up
+//POST /questions/:id/answers/:aID/vote-down
+// Vote on a specific answer
+router.post("/:qID/answers/:aID/vote-:dir",(req,res,next)=>{
+    res.json({
+        response: "You sent a Post request to vote on /answers",
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        vote:req.params.dir
+    });
+});
 module.exports = router;
