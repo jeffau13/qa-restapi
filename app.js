4f70b01571f8e4701d9e76183798662c911fbc63
jeffau13 <jeffau13@gmail.com>
2019-03-19 17:22:41 -0400

    created models for questions and answers. Implemented methods for voting and updating

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes.js');
 const jsonParser = require('body-parser').json;
+
 const logger = require('morgan');
 
 const port = process.env.PORT || 3000;
@@ -11,6 +12,21 @@ const port = process.env.PORT || 3000;
 app.use(logger('dev'));
 app.use(jsonParser());
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/qa', {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on('error', error => {
+    console.error(`connection error:`, error);
+});
+
+db.once('open', () => {
+  console.log('db connection successful');
+
+});
 app.use("/questions", routes);
 
 //catch 404 error and forward to error handler
